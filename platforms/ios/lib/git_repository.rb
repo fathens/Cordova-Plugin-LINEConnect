03891ba88d5e6c9766f594397f610e9c35a49ef5
Kunio Sawatani <devel@fathens.org>
2016-12-16 14:30:52 +0900

    Podfile や XcodeProject のライブラリを作成

---
+++ platforms/ios/lib/git_repository.rb
@@ -0,0 +1,23 @@
+require 'fileutils'
+
+module GitRepository
+    def self.clone_lineadapter(dir)
+        git_clone(
+            "https://bitbucket.org/fathens/lineadapter_ios.git",
+            "version/3.2.1",
+            ENV['BITBUCKET_USERNAME'],
+            ENV['BITBUCKET_PASSWORD'],
+            dir
+        )
+    end
+
+    def self.git_clone(url, tag, username, password, dir)
+        FileUtils.rm_rf(dir) if dir.exist?
+        cred = [username, password].map {|s| s.shellescape }.join(':')
+        target_url = url.sub(/^https:\/\//, "https://#{cred}@")
+        system "git clone #{target_url} #{dir}"
+        Dir.chdir(dir) {
+            system "git checkout #{tag}"
+        }
+    end
+end
