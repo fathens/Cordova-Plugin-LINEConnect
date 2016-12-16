b2b661bb06fcc143ce60babc7ce536cf3e81ca91
Kunio Sawatani <devel@fathens.org>
2016-12-16 10:10:51 +0900

    Bridging-Header を使うよう拡張

--- platforms/ios/generator.rb
+++ platforms/ios/generator.rb
@@ -11,29 +11,51 @@ def log_header(msg)
     log "#### #{msg}"
 end
 
-def accessors(clazz)
-    keys = clazz.instance_methods
-    names = keys.map { |key| key.to_s }
-    keys.select { |key|
-        key.to_s.match(/^\w+$/) && names.include?("#{key}=")
-    }
-end
-
-class Pod
-    attr_accessor :name, :version, :path, :git, :branch, :tag, :commit, :podspec, :subspecs
+class ElementStruct
+    def self.accessors(clazz)
+        keys = clazz.instance_methods
+        names = keys.map { |key| key.to_s }
+        keys.select { |key|
+            key.to_s.match(/^\w+$/) && names.include?("#{key}=")
+        }
+    end
 
     def initialize(params = {})
-        e = params[:element]
-        accessors(Pod).each { |key|
-            value = params[key] || (e ? e.attributes[key.to_s] : nil)
+        @element = params[:element]
+        ElementStruct.accessors(self.class).each { |key|
+            value = params[key] || attributes(key.to_s)
             send "#{key}=", value
         }
     end
 
-    def or_nil(key, with_prefix = true)
-        value = send(key)
-        prefix = with_prefix ? ":#{key} => " : ""
-        return value ? "#{prefix}'#{value}'" : nil
+    def attributes(name)
+        @element ? @element.attributes[name] : nil
+    end
+
+    def sub_elements(xpath)
+        @element ? @element.get_elements(xpath) : []
+    end
+end
+
+class BridgingHeader < ElementStruct
+    attr_accessor :import
+
+    def initialize(params = {})
+        super
+    end
+end
+
+class Pod < ElementStruct
+    attr_accessor :name, :version, :path, :git, :branch, :tag, :commit, :podspec, :subspecs
+
+    def initialize(params = {})
+        super
+    end
+
+    def bridging_headers
+        @bridging_headers ||= sub_elements('bridging-header').map { |e|
+            BridgingHeader.new(element: e)
+        }
     end
 
     def to_s
@@ -49,20 +71,27 @@ class Pod
             @subspecs ? ":subspecs => [#{@subspecs.split(',').map {|x| "'#{x.strip}'"}.join(', ')}]" : nil
         ]
         log "Pod #{args}"
-        "pod " + args.select { |a| a != nil }.join(', ')
+        "pod " + args.compact.join(', ')
+    end
+
+    private
+
+    def or_nil(key, with_prefix = true)
+        value = send(key)
+        prefix = with_prefix ? ":#{key} => " : ""
+        return value ? "#{prefix}'#{value}'" : nil
     end
 end
 
-class Podfile
-    attr_accessor :ios_version, :swift_version, :pods
+class Podfile < ElementStruct
+    attr_accessor :ios_version, :swift_version
 
-    def initialize(xmlFile)
-        xml = REXML::Document.new(File.open(xmlFile))
-        element = xml.get_elements('//platform[@name="ios"]/podfile').first
+    def initialize(params = {})
+        super
+    end
 
-        @ios_version = element.attributes['ios_version'] || '10.0'
-        @swift_version = element.attributes['swift_version'] || '3.0'
-        @pods = element.get_elements('pod').map { |e|
+    def pods
+        @pods ||= sub_elements('pod').map { |e|
             Pod.new(element: e)
         }
     end
@@ -81,6 +110,20 @@ class Podfile
                 "    #{pod}"
             }
             dst.puts "end"
+        }
+    end
+end
+
+class BridgingHeaderFile
+    def initialize(all)
+        @headers = all.uniq { |x| x.import }
+    end
+
+    def write(file)
+        File.open(file, "w") { |dst|
+            dst.puts @headers.map { |header|
+                "#import <#{header.import}>"
+            }
         }
     end
 end
@@ -124,14 +167,21 @@ $PLATFORM_DIR = Pathname($0).realpath.dirname
 $PROJECT_DIR = $PLATFORM_DIR.dirname.dirname
 $TITLE = "CordovaPlugin_#{$PROJECT_DIR.basename}"
 
-podfile = Podfile.new($PROJECT_DIR/'plugin.xml')
+plugin_xml = REXML::Document.new(File.open($PROJECT_DIR/'plugin.xml'))
+
+podfile = Podfile.new(element: plugin_xml.get_elements('//platform[@name="ios"]/podfile').first)
 podfile.pods.unshift(Pod.new(name: 'Cordova'))
+
+bridge_file = $PLATFORM_DIR/"Bridging-Header.h"
+bridge = BridgingHeaderFile.new(podfile.pods.map {|p| p.bridging_headers }.flatten)
+bridge.write(bridge_file)
 
 proj = XcodeProject.new($TITLE)
 proj.sources_pattern = "src/*.swift"
 proj.build_settings = {
-  "SWIFT_VERSION" => podfile.swift_version,
-  "ENABLE_BITCODE" => "NO"
+    "SWIFT_OBJC_BRIDGING_HEADER" => bridge_file.relative_path_from($PLATFORM_DIR),
+    "SWIFT_VERSION" => podfile.swift_version,
+    "ENABLE_BITCODE" => "NO"
 }
 
 proj.write
