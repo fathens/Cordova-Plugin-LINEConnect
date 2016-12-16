f7f64326d9e357f46f9706ef2ac55b8e1f97b641
Kunio Sawatani <devel@fathens.org>
2016-12-16 20:18:35 +0900

    git_clone をもっと汎用的に

--- lib/git_repository.rb
+++ lib/git_repository.rb
@@ -5,20 +5,18 @@ module GitRepository
     def self.clone_lineadapter(plugin_dir)
         git_clone(
             "https://bitbucket.org/fathens/lineadapter_ios.git",
-            "version/3.2.1",
-            ENV['BITBUCKET_USERNAME'],
-            ENV['BITBUCKET_PASSWORD'],
-            plugin_dir/'.tmp'/'LineAdapter-iOS'
+            plugin_dir/'.tmp'/'LineAdapter-iOS',
+            tag: "version/3.2.1",
+            username: ENV['BITBUCKET_USERNAME'],
+            password: ENV['BITBUCKET_PASSWORD']
         )
     end
 
-    def self.git_clone(url, tag, username, password, dir)
+    def self.git_clone(url, dir, tag: nil, username: nil, password: nil)
         FileUtils.rm_rf(dir) if dir.exist?
-        cred = [username, password].map {|s| s.shellescape }.join(':')
-        target_url = url.sub(/^https:\/\//, "https://#{cred}@")
+        cred = [username, password].compact.map {|s| s.shellescape }.join(':')
+        target_url = cred.empty? ? url : url.sub(/^https:\/\//, "https://#{cred}@")
+        target_url = "-b #{tag} #{target_url}" if tag
         system "git clone #{target_url} #{dir}"
-        Dir.chdir(dir) {
-            system "git checkout #{tag}"
-        }
     end
 end
