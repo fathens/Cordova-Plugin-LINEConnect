8142cffb2d10bbffddb06fb912f2e75b2adfff98
Kunio Sawatani <devel@fathens.org>
2016-12-20 21:01:16 +0900

    plugin.gradle の雛形を書き換える

--- platforms/android/hooks/after_plugin_install.rb
+++ platforms/android/hooks/after_plugin_install.rb
@@ -3,8 +3,23 @@
 require 'pathname'
 require 'fetch_local_lib'
 
-puts "Working with #{$0}"
+def rewrite_gradle(fileSrc)
+    fileDst = "#{fileSrc}.tmp"
+        open(fileSrc, 'r') { |src|
+            open(fileDst, 'w') { |dst|
+                src.each_line { |line|
+                    dst.puts line.gsub(/\$\{(.+?)\}/) {
+                        ENV[$1] || $1
+                    }
+                }
+            }
+        }
+    File.rename(fileDst, fileSrc)
+end
 
-$PLUGIN_DIR = Pathname(ENV['CORDOVA_HOOK'] || $0).realpath.dirname.dirname.dirname.dirname
+$PLUGIN_PLATFORM_DIR = Pathname(ENV['CORDOVA_HOOK'] || $0).realpath.dirname.dirname
+$PLUGIN_DIR = $PLUGIN_PLATFORM_DIR.dirname.dirname
+ENV['PLUGIN_DIR'] = $PLUGIN_DIR.to_s
 
 FetchLocalLib::Repo.bitbucket($PLUGIN_DIR, "lineadapter_android", tag: "version/3.1.21").git_clone
+rewrite_gradle $PLUGIN_PLATFORM_DIR/'plugin.gradle'
